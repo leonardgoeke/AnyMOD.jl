379aebc99ebdf06c7ad204424d42f6d5ab3807fd
leonardgoeke <leonard.goeke@campus.tu-berlin.de>
2020-07-01 19:53:51 +0200

    again rewrote part that somethis threw a mysterious error

--- src/optModel/other.jl
+++ src/optModel/other.jl
@@ -219,8 +219,7 @@ end
 # XXX create constarints that enforce any type of limit (Up/Low/Fix) on any type of variable
 function createLimitCns!(techIdx_arr::Array{Int,1},partLim::OthPart,anyM::anyModel)
 
-	println(keys(partLim.par))
-	parLim_arr = string.(collect(keys(partLim.par)))
+	parLim_arr = map(x -> string(x[1]),partLim.par)
 	techLim_arr = filter(x ->  any(map(y -> occursin(y,x),["Up","Low","Fix"])) ,parLim_arr)
 	limVar_arr = map(x -> map(k -> Symbol(k[1]) => Symbol(k[2][1]), filter(z -> length(z[2]) == 2,map(y -> y => split(x,y),["Up","Low","Fix"])))[1], techLim_arr)
 	varToPar_dic = Dict(y => getindex.(filter(z -> z[2] == y,limVar_arr),1) for y in unique(getindex.(limVar_arr,2)))
