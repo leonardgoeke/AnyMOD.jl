fee3c5fc0babbbf7020d59bc3b1fe9166de88d24
leonardgoeke <lgo@wip.tu-berlin.de>
2020-08-05 11:02:20 +0200

    moved testing model in test folder

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,8 @@
 using AnyMOD, Cbc, Test
 
 @testset "run_demo" begin
-    # manipulate set_carrier.csv to test on smaller resolution, because otherwise Cbs times out during test
-    #carrier_df = CSV.read("examples/demo/set_carrier.csv")
-    #preserveReso_arr = carrier_df[!,:timestep_dispatch]
-    #carrier_df[!,:timestep_dispatch] = min.(carrier_df[!,:timestep_dispatch],2)
-    #CSV.write("examples/demo/set_carrier.csv",carrier_df)
 
-    anyM = anyModel("examples/test","examples", objName = "test", shortExp = 10)
+    anyM = anyModel("testModel","testModel", objName = "test", shortExp = 10)
 
     plotTree(:region,anyM)
     plotEnergyFlow(:graph,anyM)
@@ -22,11 +17,6 @@ using AnyMOD, Cbc, Test
     reportResults(:summary,anyM)
     reportTimeSeries(:electricity, anyM)
     plotEnergyFlow(:sankey,anyM)
-
-    # recreates old set_carrier file
-    #carrier_df = CSV.read("examples/demo/set_carrier.csv")
-    #carrier_df[!,:timestep_dispatch] = preserveReso_arr
-    #CSV.write("examples/demo/set_carrier.csv",carrier_df)
 
     @test length(anyM.report) == 2
     @test round(objective_value(anyM.optModel),digits = 1) == 141395.1
