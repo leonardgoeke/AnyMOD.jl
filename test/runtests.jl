b05ee81500dd13b5a2cd706cf4a028cfd8591c2a
leonardgoeke <lgo@wip.tu-berlin.de>
2020-08-05 17:20:35 +0200

    adjusted test problem

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,34 @@
 
 using AnyMOD, Cbc, Test
 
-@testset "run_demo" begin
+@testset "run_rest" begin
 
-    anyM = anyModel("testModel","testModel", objName = "test", shortExp = 10)
-
-    plotTree(:region,anyM)
-    plotEnergyFlow(:graph,anyM)
+    # create model
+    anyM = anyModel("testModel","testModel", objName = "test", shortExp = 10, checkRng = 1e8)
 
     createOptModel!(anyM)
     setObjective!(:costs,anyM)
 
+    # solve model
     set_optimizer(anyM.optModel,Cbc.Optimizer)
     optimize!(anyM.optModel)
 
-    reportResults(:summary,anyM)
-    reportTimeSeries(:electricity, anyM)
-    plotEnergyFlow(:sankey,anyM)
+    # write data reports
+    reportResults(:summary,anyM, rtnOpt = (:raw,:rawDf,:csv,:csvDf))
+    reportResults(:exchange,anyM, rtnOpt = (:raw,:rawDf,:csv,:csvDf))
+    reportResults(:costs,anyM, rtnOpt = (:raw,:rawDf,:csv,:csvDf))
+    reportTimeSeries(:electricity, anyM, rtnOpt = (:raw,:rawDf,:csv,:csvDf))
+    reportTimeSeries(:electricity, anyM, rtnOpt = (:raw,:rawDf,:csv,:csvDf), mergeVar = false)
+    printDuals(anyM.parts.bal.cns[:electricity],anyM)
 
-    @test length(anyM.report) == 2
-    @test round(objective_value(anyM.optModel),digits = 1) == 141395.1
+    # create plots
+    plotEnergyFlow(:sankey,anyM, rmvNode = ("export",))
+    plotTree(:region,anyM)
+    plotEnergyFlow(:graph,anyM)
+    moveNode!(anyM,("coal",[0.1,0.1]))
+
+
+    @test length(anyM.report) == 33
+    @test round(objective_value(anyM.optModel),digits = 1) == 147521.1
+
 end
