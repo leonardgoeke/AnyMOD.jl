95caab721279e8323932459045305cc7b28e093a
leonardgoeke <lgo@wip.tu-berlin.de>
2020-08-26 19:35:33 +0200

    changed reporting on contradicting limits to not apply in case of equality

--- src/optModel/other.jl
+++ src/optModel/other.jl
@@ -302,7 +302,7 @@ function createLimitCns!(partLim::OthPart,anyM::anyModel)
 
 			# upper and lower limit contradicting each other
 			if :Low in limitCol_arr && :Up in limitCol_arr
-				for x in findall(replace(allLimit_df[!,:Low],nothing => 0.0) .>= replace(allLimit_df[!,:Up],nothing => Inf))
+				for x in findall(replace(allLimit_df[!,:Low],nothing => 0.0) .> replace(allLimit_df[!,:Up],nothing => Inf))
 					dim_str = join(map(y -> allLimit_df[x,y] == 0 ?  "" : string(y,": ",join(getUniName(allLimit_df[x,y], anyM.sets[colSet_dic[y]])," < ")),intCol(allLimit_df)),"; ")
 					lock(anyM.lock)
 					push!(anyM.report,(3,"limit",string(va),"contradicting values for upper and lower limit detected for: " * dim_str))
@@ -312,7 +312,7 @@ function createLimitCns!(partLim::OthPart,anyM::anyModel)
 
 			# fix and upper limit contradicting each other
 			if :Fix in limitCol_arr && :Up in limitCol_arr
-				for x in findall(replace(allLimit_df[!,:Fix],nothing => 0.0) .>= replace(allLimit_df[!,:Up],nothing => Inf))
+				for x in findall(replace(allLimit_df[!,:Fix],nothing => 0.0) .> replace(allLimit_df[!,:Up],nothing => Inf))
 					dim_str = join(map(y -> allLimit_df[x,y] == 0 ?  "" : string(y,": ",join(getUniName(allLimit_df[x,y], anyM.sets[colSet_dic[y]])," < ")),intCol(allLimit_df)),"; ")
 					lock(anyM.lock)
 					push!(anyM.report,(3,"limit",string(va),"fixed limit exceeds upper limit for: " * dim_str))
@@ -322,7 +322,7 @@ function createLimitCns!(partLim::OthPart,anyM::anyModel)
 
 			# fix and lower limit contradicting each other
 			if :Fix in limitCol_arr && :Low in limitCol_arr
-				for x in findall(replace(allLimit_df[!,:Fix],nothing => Inf) .<= replace(allLimit_df[!,:Low],nothing => 0.0))
+				for x in findall(replace(allLimit_df[!,:Fix],nothing => Inf) .< replace(allLimit_df[!,:Low],nothing => 0.0))
 					dim_str = join(map(y -> allLimit_df[x,y] == 0 ?  "" : string(y,": ",join(getUniName(allLimit_df[x,y], anyM.sets[colSet_dic[y]])," < ")),intCol(allLimit_df)),"; ")
 					lock(anyM.lock)
 					push!(anyM.report,(3,"limit",string(va),"fixed limit is smaller than lower limit for: " * dim_str))
