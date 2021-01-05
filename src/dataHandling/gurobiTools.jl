d36404e082803b8ac6b3ba9f8b6a13a6dd299ec3
leonardgoeke <lgo@wip.tu-berlin.de>
2021-01-05 12:12:05 +0100

    adjusted printIIS to new gurobi version

--- src/dataHandling/gurobiTools.jl
+++ src/dataHandling/gurobiTools.jl
@@ -2,16 +2,16 @@ using Gurobi
 function printIIS(anyM::anyModel)
 
     # computes iis
-    Gurobi.compute_conflict(anyM.optModel.moi_backend.optimizer.model)
+    MOI.compute_conflict!(anyM.optModel.moi_backend.optimizer.model)
 
-    if anyM.optModel.moi_backend.optimizer.model.inner.conflict != 0 return end
+    if anyM.optModel.moi_backend.optimizer.model.conflict != 0 return end
     # loops over constraint tables to find constraints within iis
-    allCns_pair = vcat(collect.(vcat(anyM.parts.bal.cns, anyM.parts.trd.cns, anyM.parts.lim.cns, map(x -> x.cns,values(anyM.parts.tech))...))...)
+    allCns_pair = vcat(collect.(vcat(anyM.parts.bal.cns, anyM.parts.cost.cns, anyM.parts.lim.cns, map(x -> x.cns,values(anyM.parts.exc))...,map(x -> x.cns,values(anyM.parts.tech))...))...)
 
     for cns in allCns_pair
         if cns[1] == :objEqn continue end
 
-        allConstr_arr = findall(map(x -> MOI.get(anyM.optModel.moi_backend, Gurobi.ConstraintConflictStatus(), x.index),cns[2][!,:cns]))
+        allConstr_arr = findall(map(x -> MathOptInterface.ConflictParticipationStatusCode(0) != MOI.get(anyM.optModel.moi_backend, MOI.ConstraintConflictStatus(), x.index),cns[2][!,:cns]))
         # prints constraints within iis
         if !isempty(allConstr_arr)
             println("$(length(allConstr_arr)) of IIS in $(cns[1]) constraints.")
