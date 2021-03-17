793267f4eebdc08457f493dc02cff834e7d9f825
leonardgoeke <lgo@wip.tu-berlin.de>
2021-03-17 21:45:11 +0100

    updated printIIS to print id columns

--- src/dataHandling/gurobiTools.jl
+++ src/dataHandling/gurobiTools.jl
@@ -6,7 +6,7 @@ function printIIS(anyM::anyModel)
 
     if anyM.optModel.moi_backend.optimizer.model.conflict != 0 return end
     # loops over constraint tables to find constraints within iis
-    allCns_pair = vcat(collect.(vcat(anyM.parts.bal.cns, anyM.parts.cost.cns, anyM.parts.lim.cns, map(x -> x.cns,values(anyM.parts.exc))...,map(x -> x.cns,values(anyM.parts.tech))...))...)
+    allCns_pair = vcat(collect.(vcat(anyM.parts.obj.cns, anyM.parts.bal.cns, anyM.parts.cost.cns, anyM.parts.lim.cns, map(x -> x.cns,values(anyM.parts.exc))...,map(x -> x.cns,values(anyM.parts.tech))...))...)
 
     for cns in allCns_pair
         if cns[1] == :objEqn continue end
@@ -18,7 +18,7 @@ function printIIS(anyM::anyModel)
             colSet_dic = Dict(x => Symbol(split(string(x),"_")[1]) for x in intCol(cns[2]))
             for iisConstr in allConstr_arr
                 row = cns[2][iisConstr,:]
-                dimStr_arr = map(x -> row[x] == 0 ?  "" : string(x,": ",join(getUniName(row[x], anyM.sets[colSet_dic[x]])," < ")),collect(keys(colSet_dic)))
+                dimStr_arr = map(x -> row[x] == 0 ?  "" : x == :id ? string(row[x]) : string(x,": ",join(getUniName(row[x], anyM.sets[colSet_dic[x]])," < ")),collect(keys(colSet_dic)))
                 println("$(join(filter(x -> x != "",dimStr_arr),", ")), constraint: $(row[:cns])")
             end
         end
