9e470ebe7451d3830711ebf5652f5c43f914f006
leonardgoeke <lgo@wip.tu-berlin.de>
2021-03-27 01:28:59 +0100

    added new types and functions for decomposition

--- src/decomposition.jl
+++ src/decomposition.jl
@@ -1,48 +1,21 @@
-#region # * functions and objects for benders specific data handling
 
-# ! struct to save benders related results
+#region # * benders objects
+
+# ! struct for results of a sub-problem
 mutable struct bendersData
 	objValue::Float64
 	capa::Dict{Symbol,Dict{Symbol,Dict{Symbol,DataFrame}}}
 	bendersData() = new(0.0,Dict{Symbol,Dict{Symbol,Dict{Symbol,DataFrame}}}())
 end
 
-# ! copy functions for benders related results
-function copy(ben_obj::bendersData)
-	out = bendersData()
-	out.objValue = ben_obj.objValue
-	out.capa = deepcopy(ben_obj.capa)
-	return out
-end
-
-# ! replaces the variable column with a column storing the value of the variable
-function getCapa(capa_df::DataFrame)
-	# filter entries without variable
-	filter!(x -> !isempty(x.var.terms),capa_df)
-	# write value of variable dataframe
-	capa_df[!,:value] = map(x -> round.(value(collect(keys(x.terms))[1]),digits = 5),capa_df[!,:var])
-	return select(capa_df,Not([:var]))
-end
-
-# ! fixes capacity variable from second dataframe according to value in the first
-function fixCapa!(value_df::DataFrame,variable_df::DataFrame)
-	if !isempty(value_df)
-		capaSub_df = leftjoin(variable_df,value_df,on = intCol(value_df,:dir))|> (y -> y[completecases(y),:])
-		foreach(x -> x.var  |> (y -> fix(collect(keys(y.terms))[1], x.value; force = true)),eachrow(capaSub_df))
-	end
-end
-
-# ! adds a dual into the first dataframe based on the matching variable column in the second
-function addDual(dual_df::DataFrame,variable_df::DataFrame)
-	new_df = leftjoin(dual_df,variable_df, on = intCol(dual_df,:dir))
-	new_df[!,:dual] = map(x -> dual(FixRef(collect(keys(x.terms))[1])), new_df[!,:var])
-	return select(filter(x -> x.dual != 0.0,new_df),Not([:var]))
-end
-
-# ! computes the capacity variable dependant expression of the benders cut from variables in the second datframe (using the dual and current value)
-function getBendersExpr(sub_df::DataFrame, variable_df::DataFrame)
-	ben_df = innerjoin(variable_df,sub_df, on = intCol(sub_df,:dir))
-	return isempty(ben_df) ? AffExpr() : sum(map(x -> x.dual *( collect(keys(x.var.terms))[1] - x.value),eachrow(ben_df)))
+# ! stores all information on current trust region 
+mutable struct trustRegion
+	objValue::Float64
+	rad::Float64
+	cns::ConstraintRef
+	capa::Dict{Symbol,Dict{Symbol,Dict{Symbol,DataFrame}}}
+	cnsInfo::NamedTuple{(:sca,:cons,:val),Tuple{Float64,Float64,Array{Float64,1}}}	
+	trustRegion() = new()
 end
 
 #endregion
@@ -105,13 +78,13 @@ function runTopLevel!(top_m::anyModel,cutData_dic::Dict{Tuple{Int64,Int64},bende
 		for sys in (:tech,:exc)
 			part_dic = getfield(top_m.parts,sys)
 			for sSym in keys(subCut.capa[sys]), capaSym in keys(subCut.capa[sys][sSym])
-				push!(cutExpr_arr,getBendersExpr(subCut.capa[sys][sSym][capaSym],part_dic[sSym].var[capaSym]))
+				push!(cutExpr_arr,getBendersCut(subCut.capa[sys][sSym][capaSym],part_dic[sSym].var[capaSym]))
 			end
 		end
 		
 		# add benders cut to top problem
 		newCut_cns = @constraint(top_m.optModel, subCut.objValue + sum(cutExpr_arr[x] for x in 1:length(cutExpr_arr)) <= filter(x -> x.Ts_disSup == top_m.supTs.step[sub[1]] && x.scr == sub[2], top_m.parts.obj.var[:cut])[1,:var])
-		top_m.parts.obj.cns[:bendersCuts] |> (x -> append!(x,DataFrame(id = i, Ts_disSup = top_m.supTs.step[sub[1]],scr = sub[2], cns = newCut_cns, binds = [Int[]])))
+		top_m.parts.obj.cns[:bendersCuts] |> (x -> append!(x,DataFrame(id = i, Ts_disSup = top_m.supTs.step[sub[1]],scr = sub[2], cns = newCut_cns)))
 	end
 
 
@@ -121,13 +94,9 @@ function runTopLevel!(top_m::anyModel,cutData_dic::Dict{Tuple{Int64,Int64},bende
 	end
 
 	# write technology capacites to BendersData object
-	for sys in (:tech,:exc)
-		capaData_obj.capa[sys] = Dict{Symbol,Dict{Symbol,DataFrame}}()
-		part_dic = getfield(top_m.parts,sys)
-		for sSym in keys(part_dic)
-			capaData_obj.capa[sys][sSym] = Dict(capaSym => getCapa(copy(part_dic[sSym].var[capaSym])) for capaSym in filter(x -> occursin("capa",string(x)), keys(part_dic[sSym].var)))
-		end
-	end
+	capaData_obj.capa = writeCapa(top_m)
+
+
 
 	topObj_fl = value(sum(filter(x -> x.name == :cost, top_m.parts.obj.var[:objVar])[!,:var]))
 	# derive lower and upper bounds on objective value
@@ -142,92 +111,93 @@ end
 
 #endregion
 
-#=
-# ! steinbruch
+#region # * sub-functions for benders decomposition
 
+# ! add trust region to objective part of model
+function centerTrustRegion(capa_dic::Dict{Symbol,Dict{Symbol,Dict{Symbol,DataFrame}}},radScal_fl::Float64,top_m::anyModel)
 
-import Base.isempty
-isempty(ben_obj::bendersData) = isempty(ben_obj.capaTech) && isempty(ben_obj.capaExc)
+	# * match capacity values with variables
+	capaExpr_dic = Dict{Symbol,Dict{Symbol,Dict{Symbol,DataFrame}}}()
+	for sys in (:tech,:exc)
+		capaExpr_dic[sys] = Dict{Symbol,Dict{Symbol,DataFrame}}()
+		part_dic = getfield(top_m.parts,sys)
+		for sSym in keys(capa_dic[sys])
+			subCapa_dic = capa_dic[sys][sSym]
+			capaExpr_dic[sys][sSym] = Dict(subCapa_dic[capaSym] |> (z -> capaSym => innerjoin(part_dic[sSym].var[capaSym],z, on = intCol(z,:dir))) for capaSym in filter(x -> occursin("capa",string(x)), keys(subCapa_dic)))
+		end
+	end
 
+	# * write trust region constraint and save its key parameters
+	
+	# get quadratic left hand side
+	allCapa_df = vcat(vcat(vcat(map(x -> capaExpr_dic[x] |> (u -> map(y -> u[y] |> (w -> map(z -> w[z][!,[:var,:value]],collect(keys(w)))),collect(keys(u)))),[:tech,:exc])...)...)...)
+	capaSum_expr = sum(map(x -> collect(keys(x.var.terms))[1] |> (z -> z^2 - 2*x.value*z + x.value^2),eachrow(allCapa_df)))
+	
+	# save scaling factor, constant of left hand side and array of capacities values
+	cnsInfo_ntup =  (sca = capaSum_expr.aff.constant, cons = top_m.options.coefRng.mat[1]/minimum(abs.(collect(values(capaSum_expr.aff.terms)))), val = allCapa_df[!,:value])
+	trustRegion_cns = @constraint(top_m.optModel,  capaSum_expr * cnsInfo_ntup.sca <= sum(map(x -> (x * radScal_fl)^2, cnsInfo_ntup.val))  * cnsInfo_ntup.sca)
 
-# ! creates variables corresponding to limits on dual in the dual problem for bundle cutting
-function createBoundElements(anyM::anyModel,variable_df::DataFrame)
-	var_obj = JuMP.build_variable(error, VariableInfo(true, 0, false, NaN, false, NaN, false, NaN, false, false))
-	cns_df = filter(x -> !isempty(x.var.terms),variable_df)
-	cns_df[!,:lowDual] = map(x -> JuMP.add_variable(anyM.optModel, var_obj, "lowDual"), 1:size(cns_df,1))
-	cns_df[!,:upDual] = map(x -> JuMP.add_variable(anyM.optModel, var_obj, "upDual"), 1:size(cns_df,1))
-	cns_df[!,:cns] = map(x -> @constraint(anyM.optModel, collect(keys(x.var.terms))[1] - x.lowDual + x.upDual == 0.0), eachrow(cns_df))
-
-	return cns_df
+	return trustRegion_cns, cnsInfo_ntup
 end
 
-# ! gets the product of dual variables for bounds on duals and the upper and lower values on the duals
-function getBoundObjective(var_dic::Dict{Symbol,DataFrame},bound_df::DataFrame,capa::Symbol)
-	# join variables for upper and lower bound
-	boundObj_df = rename(var_dic[Symbol(:ccbLow,makeUp(capa))],:var => :lowVar)
-	boundObj_df = rename(innerjoin(boundObj_df,var_dic[Symbol(:ccbUp,makeUp(capa))], on = intCol(bound_df,:dir)),:var => :upVar)
-	# add defined bounds and create expression for objective
-	boundObj_df = innerjoin(boundObj_df,bound_df, on = intCol(bound_df,:dir))
-	return sum(boundObj_df[!,:lowVar] .* boundObj_df[!,:lowBound]), sum(boundObj_df[!,:upVar] .* boundObj_df[!,:upBound])
+# ! shrings the trust region around the current center according to factor
+function shrinkTrustRegion!(trustReg_obj::trustRegion; shrFac_fl::Float64 = 0.5)
+	# reduce factor for region
+	trustReg_obj.rad = trustReg_obj.rad * shrFac_fl
+	# adjust righ hand side of constraint
+	cnsInfo_tup = trustReg_obj.cnsInfo
+	set_normalized_rhs(trustReg_obj.cns, cnsInfo_tup.cons - sum(map(x -> (x * trustReg_obj.rad)^2, cnsInfo_tup.cons.val))  * cnsInfo_tup.cons.sca )
+end
+
+# ! computes the capacity variable dependant expression of the benders cut from variables in the second datframe (using the dual and current value)
+function getBendersCut(sub_df::DataFrame, variable_df::DataFrame)
+	ben_df = innerjoin(variable_df,sub_df, on = intCol(sub_df,:dir))
+	return isempty(ben_df) ? AffExpr() : sum(map(x -> x.dual *( collect(keys(x.var.terms))[1] - x.value),eachrow(ben_df)))
+end
+
+# ! write capacities in input model to returned capacity dictionary
+function writeCapa(in_m::anyModel)
+	capa_dic = Dict{Symbol,Dict{Symbol,Dict{Symbol,DataFrame}}}()
+	for sys in (:tech,:exc)
+		capa_dic[sys] = Dict{Symbol,Dict{Symbol,DataFrame}}()
+		part_dic = getfield(in_m.parts,sys)
+		for sSym in filter(x -> part_dic[x].type in (:mature,:emerging),keys(part_dic))
+			capa_dic[sys][sSym] = Dict(capaSym => getCapa(copy(part_dic[sSym].var[capaSym])) for capaSym in filter(x -> occursin("capa",string(x)), keys(part_dic[sSym].var)))
+		end
+	end
+	return capa_dic
+end
+
+# ! copy functions for benders related results
+function copy(ben_obj::bendersData)
+	out = bendersData()
+	out.objValue = ben_obj.objValue
+	out.capa = deepcopy(ben_obj.capa)
+	return out
+end
+
+# ! replaces the variable column with a column storing the value of the variable
+function getCapa(capa_df::DataFrame)
+	# filter entries without variable
+	filter!(x -> !isempty(x.var.terms),capa_df)
+	# write value of variable dataframe
+	capa_df[!,:value] = map(x -> round.(value(collect(keys(x.terms))[1]),digits = 5),capa_df[!,:var])
+	return select(capa_df,Not([:var]))
+end
+
+# ! fixes capacity variable from second dataframe according to value in the first
+function fixCapa!(value_df::DataFrame,variable_df::DataFrame)
+	if !isempty(value_df)
+		capaSub_df = leftjoin(variable_df,value_df,on = intCol(value_df,:dir))|> (y -> y[completecases(y),:])
+		foreach(x -> x.var  |> (y -> fix(collect(keys(y.terms))[1], x.value; force = true)),eachrow(capaSub_df))
+	end
 end
 
 # ! adds a dual into the first dataframe based on the matching variable column in the second
-function addDual(dual_df::DataFrame,fixCns_df::DataFrame)
-	new_df = innerjoin(dual_df,fixCns_df, on = intCol(dual_df,:dir))
-	new_df[!,:dual] = map(x -> dual(x), new_df[!,:cns])
-	return select(new_df,Not([:cns]))
+function addDual(dual_df::DataFrame,variable_df::DataFrame)
+	new_df = leftjoin(dual_df,variable_df, on = intCol(dual_df,:dir))
+	new_df[!,:dual] = map(x -> dual(FixRef(collect(keys(x.terms))[1])), new_df[!,:var])
+	return select(filter(x -> x.dual != 0.0,new_df),Not([:var]))
 end
 
 #endregion
-
-#region # * benders related data analysis
-
-# eta parts eigentlich nur in iteration nach der standard iteratio notwendig
-function analyseCuts(bendersCut_arr::Array{Dict{Tuple{Int64,Int64},bendersData}},ccbCover::Float64)
-		
-	teCut_df =  DataFrame(var = Symbol[], Ts_expSup = Int[], Ts_disSup = Int[], R_exp = Int[], C = Int[], Te = Int[], value = Float64[], dual = Float64[])
-	excCut_df = DataFrame(Ts_disSup = Int[], R_from = Int[], R_to = Int[], C = Int[], dir = Bool[], value = Float64[], dual = Float64[])
-
-	# loop ovar all current cuts
-	for bendersCut in bendersCut_arr
-		for x in sub_tup
-			for teSym in keys(bendersCut[x].capaTech), capaSym in keys(bendersCut[x].capaTech[teSym])
-				te_df = copy(bendersCut[x].capaTech[teSym][capaSym])
-				te_df[!,:var] .= capaSym
-				if !(:C in namesSym(te_df))
-					te_df[!,:C] .= 0
-				end
-
-				append!(teCut_df,te_df)
-			end
-
-			append!(excCut_df,bendersCut[x].capaExc)
-		end
-	end
-	# aggregate data for different scenarios (value is always the same, duals are summed)
-	teCut_df = combine([:value,:dual] => (a, b) -> (value=a[1], dual=sum(b)),groupby(teCut_df,intCol(teCut_df,:var)))
-	excCut_df  = combine([:value,:dual] => (a, b) -> (value=a[1], dual=sum(b)),groupby(excCut_df,intCol(excCut_df)))
-
-	# density of cut
-	density_fl = (length(findall(teCut_df[!,:dual] .!= 0.0)) + length(findall(excCut_df[!,:dual] .!= 0.0))) / (size(teCut_df,1) + size(excCut_df,1))
-	produceMessage(top_mod.options,top_mod.report, 1," - Density of cut: $(round(density_fl*100,digits = 3))%")
-	# share of variables that are alpha covered
-	allDual_arr = abs.(vcat(teCut_df[!,:dual],excCut_df[!,:dual]))
-	maxDual_fl = maximum(allDual_arr)
-	alphaCover_fl = length(findall(allDual_arr .>= maxDual_fl * ccbCover)) / (size(teCut_df,1) + size(excCut_df,1))
-	produceMessage(top_mod.options,top_mod.report, 1," - Alpha covered of cut: $(round(alphaCover_fl*100,digits = 2))%")
-	
-	cbbEta_fl = mean(allDual_arr[findall(allDual_arr .>= maxDual_fl * ccbCover)])
-	produceMessage(top_mod.options,top_mod.report, 1," - Eta of cut: $(round(cbbEta_fl,;sigdigits=3))")
-
-	# determine a random capacity not alpha covered currently,# TODO bisher nur technologies
-	#noCover = filter(x -> abs.(x.dual) < maxDual_fl * ccbCover,eachrow(teCut_df))[1,:]
-
-	return cbbEta_fl, alphaCover_fl
-
-end
-
-#endregion
-
-
-=#
