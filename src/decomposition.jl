ebc8650d4ee6c419e5742f5bba36d173899ff9f1
leonardgoeke <lgo@wip.tu-berlin.de>
2021-09-29 14:04:53 +0200

    adjusted mip gap

--- src/decomposition.jl
+++ src/decomposition.jl
@@ -223,7 +223,7 @@ function computeFeas(top_m::anyModel,var_dic::Dict{Symbol,Dict{Symbol,Dict{Symbo
 	@objective(top_m.optModel, Min, objExpr_df[1,:cnsExpr])
 	# solve problem
 	set_optimizer_attribute(top_m.optModel, "NumericFocus", 3)
-	set_optimizer_attribute(top_m.optModel, "MIPGap", 0)
+	set_optimizer_attribute(top_m.optModel, "MIPGap", 0.0001)
 	optimize!(top_m.optModel)
 	checkIIS(top_m)
 
