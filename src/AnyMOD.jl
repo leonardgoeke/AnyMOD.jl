4e0f3be6033c61ea5f563018b68ac5bc15c49a2e
leonardgoeke <lgo@wip.tu-berlin.de>
2022-01-02 14:00:42 +0100

    added YAML dependency

--- src/AnyMOD.jl
+++ src/AnyMOD.jl
@@ -18,7 +18,7 @@ module AnyMOD
         ENV["PYTHON"] = envPy
     end
 
-    using Base.Threads, CSV, Dates, LinearAlgebra, Requires, DelimitedFiles
+    using Base.Threads, CSV, Dates, LinearAlgebra, Requires, DelimitedFiles, YAML
     using MathOptInterface, Reexport, Statistics, PyCall, SparseArrays, Suppressor
     @reexport using DataFrames, JuMP, Dates, Suppressor
 
