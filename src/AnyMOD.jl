41152faf961f8e8e4ae2ba27a20c6dde295409d5
Leonard Göke <lgoeke@ethz.ch>
2024-03-12 12:56:43 +0100

    export heuristicSolve

--- src/AnyMOD.jl
+++ src/AnyMOD.jl
@@ -34,7 +34,7 @@ module AnyMOD
     export reportResults, reportTimeSeries, printObject, printDuals, computeResults, writeParameterFile!, plotGraphYML, convertYAML2GEXF # write results
     export plotTree, plotSankeyDiagram, plotNetworkGraph, moveNode!, produceMessage, produceMessageShort # reporting
     export intCol, getAllVariables, collapseExp, createVar, defineParameter, makeUp, removeEmptyDic! # low-level data management
-    export evaluateHeu, getFeasResult, writeFixToFiles, exportDesignFactors! # functions for heuristic pre-solves
+    export heuristicSolve, evaluateHeu, getFeasResult, writeFixToFiles, exportDesignFactors! # functions for heuristic pre-solves
     
     # stochastic optimization
     export algSetup, stabSetup, nearOptSetup, bendersObj, resData # objects
