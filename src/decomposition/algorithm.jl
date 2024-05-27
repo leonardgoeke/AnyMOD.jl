efac6e76ad8e4aad098408ad59cbe7385dd5a5b0
Leonard Göke <lgoeke@ethz.ch>
2024-05-27 14:49:16 +0200

    changed scope of suppress for reporting

--- src/decomposition/algorithm.jl
+++ src/decomposition/algorithm.jl
@@ -261,9 +261,9 @@ end
 # build sub-problems
 function buildSub(id::Int, genSetup_ntup::NamedTuple{(:name, :frsLvl, :supTsLvl, :repTsLvl, :shortExp), Tuple{String, Int64, Int64, Int64, Int64}}, inputFolder_ntup::NamedTuple{(:in, :heu, :results), Tuple{Vector{String}, Vector{String}, String}}, scale_dic::Dict{Symbol,NamedTuple}, algOpt_obj::algSetup)
 	# create sub-problems
-	sub_m = @suppress anyModel(inputFolder_ntup.in, inputFolder_ntup.results, objName = "subModel_" * string(id) * "_" * genSetup_ntup.name, frsLvl = genSetup_ntup.frsLvl, repTsLvl = genSetup_ntup.repTsLvl, supTsLvl = genSetup_ntup.supTsLvl, shortExp = genSetup_ntup.shortExp, coefRng = scale_dic[:rng], scaFac = scale_dic[:facSub], dbInf = algOpt_obj.solOpt.dbInf, reportLvl = 1)
+	sub_m = anyModel(inputFolder_ntup.in, inputFolder_ntup.results, checkRng = (print = true, all = false), objName = "subModel_" * string(id) * "_" * genSetup_ntup.name, frsLvl = genSetup_ntup.frsLvl, repTsLvl = genSetup_ntup.repTsLvl, supTsLvl = genSetup_ntup.supTsLvl, shortExp = genSetup_ntup.shortExp, coefRng = scale_dic[:rng], scaFac = scale_dic[:facSub], dbInf = algOpt_obj.solOpt.dbInf, reportLvl = 1)
 	sub_m.subPro = tuple([(x.Ts_dis, x.scr) for x in eachrow(sub_m.parts.obj.par[:scrProb].data)]...)[id]
-	@suppress prepareMod!(sub_m, algOpt_obj.opt, algOpt_obj.threads)
+	prepareMod!(sub_m, algOpt_obj.opt, algOpt_obj.threads)
 	set_optimizer_attribute(sub_m.optModel, "Threads", algOpt_obj.threads)
 	return sub_m
 end
@@ -437,20 +437,18 @@ function runSub(sub_m::anyModel, resData_obj::resData, sol_sym::Symbol, optTol_f
 	#region # * solve problem
 
 	# set optimizer attributes and solves
-	@suppress begin
-		if sol_sym == :barrier
-			set_optimizer_attribute(sub_m.optModel, "Method", 2)
-			set_optimizer_attribute(sub_m.optModel, "Crossover", crsOver_boo ? 1 : 0)
-			set_optimizer_attribute(sub_m.optModel, "BarOrder", 1)
-			set_optimizer_attribute(sub_m.optModel, "BarConvTol", optTol_fl)
-		elseif sol_sym == :simplex
-			set_optimizer_attribute(sub_m.optModel, "Method", 1)
-			set_optimizer_attribute(sub_m.optModel, "Threads", 1)
-			set_optimizer_attribute(sub_m.optModel, "OptimalityTol", optTol_fl)
-			set_optimizer_attribute(sub_m.optModel, "Presolve", 2)
-		end
-		optimize!(sub_m.optModel)
+	if sol_sym == :barrier
+		set_optimizer_attribute(sub_m.optModel, "Method", 2)
+		set_optimizer_attribute(sub_m.optModel, "Crossover", crsOver_boo ? 1 : 0)
+		set_optimizer_attribute(sub_m.optModel, "BarOrder", 1)
+		set_optimizer_attribute(sub_m.optModel, "BarConvTol", optTol_fl)
+	elseif sol_sym == :simplex
+		set_optimizer_attribute(sub_m.optModel, "Method", 1)
+		set_optimizer_attribute(sub_m.optModel, "Threads", 1)
+		set_optimizer_attribute(sub_m.optModel, "OptimalityTol", optTol_fl)
+		set_optimizer_attribute(sub_m.optModel, "Presolve", 2)
 	end
+	optimize!(sub_m.optModel)
 	checkIIS(sub_m)
 
 	# write results into files (only used once optimum is obtained)
