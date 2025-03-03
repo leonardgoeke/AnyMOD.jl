ec9b468c85559ec48baebb976d9c5c9cec8528ed
leonardgoeke <lgoeke@ethz.ch>
2025-03-03 12:53:20 +0100

    bugfix

--- src/tools.jl
+++ src/tools.jl
@@ -209,7 +209,10 @@ function reportResults(objGrp::Val{:summary}, anyM::anyModel; addObjName::Bool=t
 			dem_df[!,:id] .= 0
 			dem_df[!,:variable] .= :demand
 			if wrtSgn dem_df[!,:value] = dem_df[!,:value] .* -1 end
-			append!(allData_df, flatten(select(dem_df, intersect(namesSym(dem_df),namesSym(allData_df))), :R_dis))
+
+			if :Ts_frs in namesSym(allData_df) && !(:Ts_frs in namesSym(dem_df)) dem_df[!,:Ts_frs] .= 0 end
+
+			append!(allData_df, flatten(dem_df, :R_dis))
 		end
 	end
 
